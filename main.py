d4c1a128082947059a2063531a613e964daf4a00
furkan <furkanbugracelik10@gmail.com>
2026-04-19 02:14:05 +0300

    add requirements

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pyqrcode
 import png
 
+
 mail=st.text_input("Lütfen mail adresinizi giriniz:")
 telefon="905054407855"
 link=("https://wa.me/"+telefon+"?text="+mail)
@@ -9,4 +10,5 @@ qr=pyqrcode.create(link)
 qr.png("wa.png",scale="12")
 
 st.image("wa.png")
+
 
